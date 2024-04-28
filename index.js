f0fa1f6cb327e6d5119bb58bf1e8e0a3ee7fd7bf
ajay4985622 <ajay@vitto.money>
2024-04-28 16:16:04 +0530

    fixed route issue

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ app.use(bodyParser.json({limit: '30mb', extended: true}));
 app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
 app.use(cors());
 
+
+app.use("/posts", postRoutes);
 app.use("/", (req, res) => {
     res.send("Welcome to Memories API");
 })
-app.use("/posts", postRoutes);
-
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL)
